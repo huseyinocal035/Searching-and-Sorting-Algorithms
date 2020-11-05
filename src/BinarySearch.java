f4fd8075f98a57b1536254355274df2d25a183b7
Hüseyin Öcal <huseyin.ocal@tubitak.gov.tr>
2020-11-05 17:42:30 +0300

    Some problems handled in binary search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,9 +1,8 @@
 public class BinarySearch {
 
     public int findNumber(int[] array, int target, int low, int high) {
-        if (high > low ) {
+        if (high >= low ) {
             int middle = (low + high) / 2;
-
             if (target == array[middle] ) {
                 return middle;
             } else if (target < array[middle] ) {
@@ -11,14 +10,13 @@ public class BinarySearch {
             }
             return findNumber(array, target, middle+1, high);
         }
-
         return -1;
     }
 
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         int[] array = { 5,7,9,15,23,53,77};
-        int place = binarySearch.findNumber(array, 9, 0, array.length-1);
-        System.out.println(place);
+        int place = binarySearch.findNumber(array, 7454, 0, array.length-1);
+        System.out.println("Place of target is " + (place+1));
     }
 }
